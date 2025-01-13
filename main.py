af1f6d1b47bc6a20c3b1b64a873fc88af58c9b3e
josh-hensley <me@joshhensley.com>
2025-01-12 20:26:25 -0600

    figured out thrust vectors

--- main.py
+++ main.py
@@ -7,28 +7,45 @@ SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 SCREEN_CENTER = pygame.Vector2(SCREEN_WIDTH // 2, SCREEN_HEIGHT //2)
 BLACK = pygame.Color(0,0,0)
 WHITE = pygame.Color(255,255,255)
+MAX_SPEED = .1
 
 class Player:
     def __init__(self):
         self.center = SCREEN_CENTER
         self.angle = 0
-        self.rotation = 0.1
-        self.points = [
-            pygame.Vector2(self.center.x, self.center.y - 8), 
-            pygame.Vector2(self.center.x - 4, self.center.y + 8), 
-            pygame.Vector2(self.center.x + 4, self.center.y + 8)
-            ]
-        self.dx = 0.01
-        self.dy = 0.01
-    def update(self):
+        self.rotation = 1
         self.points = [
             pygame.Vector2(0, -8).rotate(self.angle) + self.center, 
             pygame.Vector2(-4, 8).rotate(self.angle) + self.center, 
             pygame.Vector2(4, 8).rotate(self.angle) + self.center
             ]
+        self.velocity = pygame.Vector2(0,0)
+        self.dx = 0
+        self.dy = 0
+    def update(self):
+        self.points = [
+            pygame.Vector2(0, -8).rotate(self.angle) + self.center, 
+            pygame.Vector2(-4, 8).rotate(self.angle) + self.center, 
+            pygame.Vector2(4, 8).rotate(self.angle) + self.center 
+            ]
     def draw(self):
         self.update()
         pygame.draw.polygon(SCREEN, WHITE, self.points, 1)
+    def thrust(self):
+        self.velocity = pygame.Vector2(0,-1).rotate(self.angle)
+        self.dy = self.dy + 0.00001 * self.velocity.y if self.dy <= MAX_SPEED else MAX_SPEED * self.velocity.y
+        self.dx = self.dx + 0.00001 * self.velocity.x if self.dx <= MAX_SPEED else MAX_SPEED * self.velocity.x
+    def move(self):
+        self.center.x += self.dx
+        self.center.y += self.dy
+        if self.center.y > SCREEN_HEIGHT:
+            self.center.y = 0
+        if self.center.y < 0:
+            self.center.y = SCREEN_HEIGHT
+        if self.center.x > SCREEN_WIDTH:
+            self.center.x = 0
+        if self.center.x < 0:
+            self.center.x = SCREEN_WIDTH
 
 def draw(p1):
     SCREEN.fill(BLACK)
@@ -36,8 +53,15 @@ def draw(p1):
     pygame.display.update()
 
 def move(p1):
-    p1.center.y -= p1.dy
-    p1.angle += p1.rotation
+    mouse_move = pygame.mouse.get_rel()
+    if mouse_move[0] > 0:
+        p1.angle += p1.rotation
+    elif mouse_move[0] < 0:
+        p1.angle -= p1.rotation
+    button = pygame.mouse.get_pressed()
+    if button[0] == True:
+        p1.thrust()
+    p1.move()
 
 def collide():
     pass
